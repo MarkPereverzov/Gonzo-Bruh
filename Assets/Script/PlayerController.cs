5c93df350f9a36f2e11cae3c7c5adebe61d66d6f
OzzeY72 <nabacap72@gmail.com>
2023-03-17 21:09:36 +0100

    First script

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody rigidbody;
-    public float speed = 2f;
+    public float speed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -20,11 +21,15 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        animator
+        //animator
 
         Vector3 directionVector = new Vector3(v, 0, h);
-        rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
+        Vector3 pos = new Vector3(rigidbody.position.x, rigidbody.position.y, rigidbody.position.z+speed);
 
+
+        animator.speed = 2;
+        rigidbody.MovePosition(pos);   
+        //rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
 
     }
 }
