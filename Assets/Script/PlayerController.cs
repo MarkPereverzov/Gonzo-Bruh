25684b63b0c2da0da68d07597d6d7936b86b04c1
markpereverzov <mark.pereverzov@gmail.com>
2023-03-18 16:33:12 +0200

    initial ban

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody rigidbody;
-    public float speed = 2f;
-    public float rotationSpeed = 10f;
+    public float speed = 4f;
+    public float rotationSpeed = 2f;
 
     void Start()
     {
@@ -16,11 +16,13 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float v = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Vertical");
 
         Vector3 directionVector = new Vector3(-v, 0, h);
+        if(directionVector.magnitude > Mathf.Abs(0.05f)) {
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * rotationSpeed);
+        }
 
         animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
         rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
