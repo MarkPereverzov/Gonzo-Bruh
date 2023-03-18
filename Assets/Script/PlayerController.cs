3a10b6eaca6ae6477f218284b89bcdecfe364c0c
markpereverzov <mark.pereverzov@gmail.com>
2023-03-18 13:35:02 +0200

    hahahahaaha

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,20 +7,21 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private Rigidbody rigidbody;
     public float speed = 2f;
-    // Start is called before the first frame update
+    public float rotationSpeed = 10f;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 directionVector = new Vector3(v, 0, h);
+        Vector3 directionVector = new Vector3(-v, 0, h);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * rotationSpeed);
+
         animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
         rigidbody.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
     }
