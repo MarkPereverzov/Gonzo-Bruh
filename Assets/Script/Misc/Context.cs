d69b18f1c5e8900bd0db8d82545697e3f3122a59
OzzeY <nabacap72@gmail.com>
2023-07-30 18:55:46 +0200

    Fixed Canister

---
+++ Assets/Script/Misc/Context.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Context : MonoBehaviour
+{
+    public CommonDevice currentDevice;
+    public Interact human;
+    public Interactable hand;
+}
